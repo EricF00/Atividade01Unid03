049eccd4c959bc87a633c1574c84d516b4abad3a
Eric Fernandes <168876546+EricF00@users.noreply.github.com>
2024-10-31 22:41:06 -0300

    Update Atividade04.py

--- Atividade04.py
+++ Atividade04.py
@@ -1,1 +1,21 @@
+# -*- coding: latin1 -*-
 
+#Programa de implementar uma função que retorne verdadeiro se o número for primo(falso caso contrário). Testar de 1 a 100.
+def verificaPrimo(num):
+    bPrimo = True
+    if num >= 2:
+        for i in range (2,num):
+            if (num % i == 0):
+                bPrimo = False
+                break
+    else:
+        bPrimo = False
+
+    return bPrimo
+
+valueInput = int(input("Digite um número inteiro para verificar se é primo: "))
+primo = verificaPrimo(valueInput)
+if primo :
+    print(f"{valueInput} é primo!")
+else:
+    print(f"{valueInput} não é primo!")
