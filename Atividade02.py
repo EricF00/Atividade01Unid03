cc99f8849f26284fb67cc632228e012f88f0d959
Eric Fernandes <168876546+EricF00@users.noreply.github.com>
2024-10-31 22:34:46 -0300

    Update Atividade02.py

--- Atividade02.py
+++ Atividade02.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: latin1 -*-
 
 #Programa que lê 3 valores a,b,c e verifica se eles formam ou não um triângulo. Supor que os valores lidos são inteiros e positivos. Caso os valores formem um triângulo, calcular e escrever a área deste triângulo. Se não formam triângulo escrever os valores lidos. (Se a &gt; b + c não formam triângulo algum, se a é o maior).
