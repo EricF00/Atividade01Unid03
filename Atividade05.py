c11295995648aa0529d207a69c1f5340f247a5e9
Eric Fernandes <168876546+EricF00@users.noreply.github.com>
2024-10-31 22:56:33 -0300

    Update Atividade05.py

--- Atividade05.py
+++ Atividade05.py
@@ -1,1 +1,12 @@
+# -*- coding: latin1 -*-
 
+#Função que receba uma frase como parâmetro e que retorne uma nova frase com cada palavra com as letras invertidas.
+def invertText(text):
+    invertedText = ""
+    for palavra in text.split(" "):
+        invertedText += palavra[::-1]+" "
+    return invertedText
+
+Text = input("Escreva uma frase que deseja inverter: ")
+invertedText = invertText(Text)
+print(invertedText)
