882ddc840ab2fe6a021006f01bd6f991b2cd1e17
Eric Fernandes <168876546+EricF00@users.noreply.github.com>
2024-10-31 22:35:16 -0300

    Update Atividade03.py

--- Atividade03.py
+++ Atividade03.py
@@ -1,1 +1,10 @@
+# -*- coding: latin1 -*-
 
+#Programa que leia 3 números inteiros e mostre o menor deles.
+arrayNumero = []
+
+valor1 = arrayNumero.append(int(input("Digite o primeiro número inteiro: ")))
+valor2 = arrayNumero.append(int(input("Digite o segundo número inteiro: ")))
+valor3 = arrayNumero.append(int(input("Digite o terceiro número inteiro: ")))
+
+print(f"O menor número é: {min(arrayNumero)}")
